f18b51c52f4f930f52d52238b532dc7eb6759a6b
Nehemie-Jacques <nehemiesighe1@gmail.com>
2025-02-28 17:58:00 +0100

    first-commit

---
+++ src/Exercice1.js
@@ -0,0 +1,50 @@
+// Exercices 7
+let nom = prompt("veuillez entrer votre nom:");
+alert("bienvenu" + nom + "!")
+console.log("bienvenu" + nom + "!")
+
+// Exercice 6
+let participants = ["Junior", "Tony", "Ange", "Manuella"];
+participants.push("Robert");
+console.log("La liste des participants:", participants);
+
+//  Exercice 5
+let Nom = "Dupont";
+let age = 33; 
+let inscriptionpayee = false;
+
+console.log("Type de nom est:", typeof Nom);
+console.log("Type de l'age est:", typeof age);
+console.log("Type de inscriptionpayee:", typeof inscriptionpayee);
+
+// Exercice 4 
+let x = Number(prompt("Premiere note:"));
+let y = Number(prompt("Deuxieme note:"));
+let z = Number(prompt("Troisieme note:"));
+let moyenne = (x + y + z) / 3;
+console.log("La moyenne des notes est:", moyenne);
+
+// Exercice 3
+let ageutilisateur = 15;
+function reduction(age) {
+    if (ageutilisateur < 18) {
+        console.log("Vous etes eligible pour la reduction");
+    }
+    else {
+        console.log("Vous n'etes pas eligible pour la reduction");
+    }
+}
+reduction(ageutilisateur);
+
+// Exercice 2
+let coutParParticipant = 2500;
+let nombreDeParticipant = 5; 
+function couttotal (coutParParticipant, nombreDeParticipant) {
+    return coutParParticipant * nombreDeParticipant;
+}
+console.log("Le coût total des inscriptions est de " + couttotal + "FCFA.");
+
+// Exercice 1
+let nomUtilisateur = prompt("Veuillez entrez votre nom :");
+const ageUtilisateur = prompt("Veuillez entrez votre âge :");
+console.log(`Bienvenue, ${nomUtilisateur} ! Vous avez ${ageUtilisateur} ans.`);
---
+++ src/Exercice2.js
@@ -0,0 +1,70 @@
+// Exercice 1
+// Questin 1
+function calculateSum(a, b) {
+    return a + b;
+}
+let a = 5;
+let b = 7;
+let somme = calculateSum(a, b);
+console.log("Le resultat est:",somme);
+
+// Question 2
+function calculateAverage([a, b, c]) {
+    return console.log((a + b + c)/3)
+}
+console.log(calculateAverage([1, 2, 3]));
+
+// Question 3
+function convertToUpperCase(hello) {
+    return hello.toUpperCase();
+}
+console.log(convertToUpperCase("hello word"));
+
+// Exercice 2
+// Question 1
+function calculateRectangleArea(longueur, largeur) {
+    return longueur * largeur;
+}
+let longueur = ("Entrez la longueur du rectangle :");
+let largeur = ("Entrez la largeur du rectangle :");
+
+console.log("L'aire du rectangle est:" + calculateRectangleArea(longueur, largeur));
+
+// Question 2
+function isEven(nombre) {
+    return nombre % 2 == 0;
+}
+let nombre = ("Entrez le nombre:");
+console.log(isEven(nombre));
+
+// Question 3
+function findElement(tableau, element) {
+    return tableau.includes(element);
+}
+console.log(findElement([10, 27, 59, 60], 3));
+
+// Exercice 3
+// Question 1
+function fibonacci(n) {
+    if (n <= 0) {
+        return "n doit être un entier positif";
+    } else if (n === 1) {
+        return 0;
+    } else if (n === 2) {
+        return 1;
+    }
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+console.log(fibonacci(5));
+
+// Question 2
+function factorial(n) {
+    if (n < 0) {
+        return "n doit être un entier positif";
+    } else if (n === 0 || n === 1) {
+        return 1; 
+    }
+    return n * factorial(n - 1);
+}
+
+console.log(factorial(9)); 
